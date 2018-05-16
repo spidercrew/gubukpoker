aaa3fb1a3e1b51759625127d1444a409e8043a99
spidercrew <baimek88@gmail.com>
2018-05-17 03:04:19 +0700

    Update nav-mobile.js

--- nav-mobile.js
+++ nav-mobile.js
@@ -1,3 +1,29 @@
 //<![CDATA[
+$(function(){ // document ready
+   if ($('#sticky').length) { // make sure "#sticky" element exists
+      var el = $('#sticky');
+      var stickyTop = $('#sticky').offset().top; // returns number
+      var stickyHeight = $('#sticky').height();
+
+      $(window).scroll(function(){ // scroll event
+          var limit = $('#footer').offset().top - stickyHeight - 5;
+
+          var windowTop = $(window).scrollTop(); // returns number
+
+          if (stickyTop < windowTop){
+             el.css({ position: 'fixed', top: 5 });
+          }
+          else {
+             el.css('position','static');
+          }
+
+          if (limit < windowTop) {
+          var diff = limit - windowTop;
+          el.css({top: diff});
+          }
+        });
+   }
+});
+//
 var ww=document.body.clientWidth;$(document).ready(function(){$(".nav li a").each(function(){if($(this).next().length>0){$(this).addClass("parent")}});$(".toggleMenu").click(function(e){e.preventDefault();$(this).toggleClass("active");$(".nav").toggle()});adjustMenu()});$(window).bind("resize orientationchange",function(){ww=document.body.clientWidth;adjustMenu()});var adjustMenu=function(){if(ww<768){$(".toggleMenu").css("display","inline-block");if(!$(".toggleMenu").hasClass("active")){$(".nav").hide()}else{$(".nav").show()}$(".nav li").unbind("mouseenter mouseleave");$(".nav li a.parent").unbind("click").bind("click",function(e){e.preventDefault();$(this).parent("li").toggleClass("hover")})}else if(ww>=768){$(".toggleMenu").css("display","none");$(".nav").show();$(".nav li").removeClass("hover");$(".nav li a").unbind("click");$(".nav li").unbind("mouseenter mouseleave").bind("mouseenter mouseleave",function(){$(this).toggleClass("hover")})}}
 //]]>
